cb88310b6265c959323e15d08f20d20af83fe385
Your Name <you@example.com>
2023-04-15 14:20:52 +0600

    Update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def contexts():
     def esc_url(url:str):
         url_path = urlsplit(url).path + '?' + urlsplit(url).query if urlsplit(url).query else urlsplit(url).path
         url = request.url_root[:-1] + url_path if request.url_root.endswith('/') else request.url_root + url_path
+        url = url.replace('https:','').replace('http:','')
         return url
 
     return dict(esc_url=esc_url)
