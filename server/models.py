59fd084cfd1f3ffd3831b4e2df45eec25b3454b3
Your Name <you@example.com>
2023-04-15 23:08:13 +0600

    Update

--- server/models.py
+++ server/models.py
@@ -1,12 +1,28 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy(engine_options={"pool_recycle": 55})
+
+class Redirect_Types(Enum):
+    HTTP = 'HTTP'
+    META = 'META'
+    SCRIPT = 'SCRIPT'
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    urls = db.relationship('urls', backref='user', lazy=True)
 
+class Short_URL(db.Model):
+    __tablename__ = 'urls'
+    id = db.Column(db.Integer, primary_key=True)
+    slug_text = db.Column(db.String(255), unique=True)
+    short_url = db.Column(db.String(255))
+    org_url = db.Column(db.String(2048), nullable=False)
+    visits = db.Column(db.Integer, default=0, nullable=False)
+    redirect_type = db.Column(db.Enum(Redirect_Types), nullable=False, default=Redirect_Types.HTTP)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
--- static/script.js
+++ static/script.js
@@ -8,3 +8,49 @@ window.onscroll = function() {
   }
   prevScrollPos = currentScrollPos;
 };
+
+function sleep(s) {
+  return new Promise(resolve => setTimeout(resolve, s*1000));
+}
+
+async function run_animated_tags() {
+  let all_tags = [
+    "Transforming long links into short and sweet URLs",
+    "Get more out of your links with our URL shortener",
+    "Say more with less - shorten your URLs today",
+    "Effortlessly share your links with our URL shortener",
+    "Simplify your links with our URL shortening service",
+    "Shorten URLs, share more, worry less",
+    "Make your links stand out with our URL shortener",
+    "Short and sweet - the power of our URL shortener",
+    "Shorten, share, succeed with our URL shortening tool",
+    "Elevate your link game with our URL shortener",
+    "Get your message across faster with our URL shortening service",
+    "Less is more - shorten your links with us",
+    "Shorten your URLs, lengthen your reach",
+  ];
+  while (true) {
+    for(let tag of all_tags){
+      for(let char of $('#animated-tags').text()){
+        $('#animated-tags').text($('#animated-tags').text().slice(0, -1));
+        await sleep(0.03);
+      };
+      for(let char of tag){
+        $('#animated-tags').text($('#animated-tags').text() + char);
+        await sleep(0.03);
+      };
+      await sleep(3);
+    };
+  };
+};
+
+
+// DEVELOPMENT CODE
+function reloadWebPage() {
+  fetch(window.location.href).then(resp => resp.text()).then( html => document.open() && document.write(html) && document.close());
+}
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.keyCode === 82) {
+    event.preventDefault();reloadWebPage();
+  }
+});
